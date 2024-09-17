eb3d2eff6270a951e90af05b2da108236e584842
Ramy <mennawalid080@gmail.com>
2024-09-17 17:38:24 +0300

    app init

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
- import 'package:flutter/material.dart';
-
-void main(){
+import 'package:flutter/material.dart';
+import 'package:islami/home.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+void main() {
   runApp(MyApp());
- }
- class MyApp extends StatelessWidget {
-   const MyApp({super.key});
+}
 
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-     );
-   }
- }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
